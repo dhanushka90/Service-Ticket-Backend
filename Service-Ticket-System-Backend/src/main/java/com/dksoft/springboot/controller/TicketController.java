ac87f174d287f89e7fa9ffe55141999e20801b37
dhanushka90 <66157527+dhanushka90@users.noreply.github.com>
2023-04-08 00:51:46 -0400

    new updates with del, put, and get by id

--- Service-Ticket-System-Backend/src/main/java/com/dksoft/springboot/controller/TicketController.java
+++ Service-Ticket-System-Backend/src/main/java/com/dksoft/springboot/controller/TicketController.java
@@ -1,15 +1,22 @@
 package com.dksoft.springboot.controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.dksoft.springboot.exception.ResourceNotFoundException;
 import com.dksoft.springboot.model.Ticket;
 import com.dksoft.springboot.reposiitory.TicketRepository;
 
@@ -31,6 +38,48 @@ public class TicketController {
 	@PostMapping("/tickets")
 	public Ticket createTicket(@RequestBody Ticket ticket) {
 		return ticketRepository.save(ticket);
+	}
+	
+	// get ticket by id
+	@GetMapping("/tickets/{id}")
+	public ResponseEntity<Ticket> getTicketById(@PathVariable Long id) {
+		
+		Ticket ticket = ticketRepository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("There is no ticket exist with id:" +id));
+		
+		return ResponseEntity.ok(ticket);
+	}
+	
+	// update ticket
+	@PutMapping("/tickets/{id}")
+	public ResponseEntity<Ticket> updateTicket(@PathVariable Long id, @RequestBody Ticket ticketDetails){
+		
+		Ticket ticket = ticketRepository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("There is no ticket exist with id:" +id));
+		
+		ticket.setTitle(ticketDetails.getTitle());
+		ticket.setDescription(ticketDetails.getDescription());
+		ticket.setFirstName(ticketDetails.getFirstName());
+		ticket.setLastName(ticketDetails.getLastName());
+		ticket.setEmail(ticketDetails.getEmail());
+		ticket.setMobile(ticketDetails.getMobile());
+		ticket.setResolution(ticketDetails.getResolution());
+		
+		Ticket updatedTicket = ticketRepository.save(ticket);
+		return ResponseEntity.ok(updatedTicket);
+	}
+
+	// delete ticket
+	@DeleteMapping("/tickets/{id}")
+	public ResponseEntity<Map<String, Boolean>> deleteTicket(@PathVariable Long id) {
+		
+		Ticket ticket = ticketRepository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("There is no ticket exist with id:" +id));
+		
+		ticketRepository.delete(ticket);
+		Map<String, Boolean> response = new HashMap<>();
+		response.put("deleted", Boolean.TRUE);
+		return ResponseEntity.ok(response);
 	}
 
 }
